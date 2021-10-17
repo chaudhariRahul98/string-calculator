93090ca922cb3a438eee00db5b268a11efb7cc84
D6_49561_rahulchaudhari <chaudhari.rc.rahul@gmail.com>
2021-10-17 19:10:39 +0530

    Created String calculator with a method signature

--- string-calculator-starter-master/src/calculator/StringCalculator.java
+++ string-calculator-starter-master/src/calculator/StringCalculator.java
@@ -1,9 +1,20 @@
 package calculator;
 
-class StringCalculator {
+public class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	int returnValue = 0;
+    	if(input.isEmpty()) {
+    		return returnValue;
+    	}else if (input.contains(",")||input.contains("/n")){
+    		String[] numbersArray = input.split(",|/n");
+    		for (String string : numbersArray) {
+    			int num =Integer.parseInt(string);
+    			returnValue +=  num ;
+    		}
+    	}else {
+    		return Integer.parseInt(input);
+    	}
+    	return returnValue;	
     }
-
 }
--- string-calculator-starter-master/src/calculator/StringCalculatorShould.java
+++ string-calculator-starter-master/src/calculator/StringCalculatorShould.java
@@ -4,17 +4,22 @@ import org.junit.*;
 
 import static org.junit.Assert.*;
 
-class StringCalculatorShould {
+public class StringCalculatorShould {
 
     @Test
-    void empty_string_should_return_0() {
+    public void empty_string_should_return_0() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
+    public void string_with_single_number_should_return_number_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
+    }
+    @Test
+    public void string_with_two_number_should_return_number_as_int() {
+    	StringCalculator stringCalculator = new StringCalculator();
+    	 assertEquals(3, stringCalculator.add("1,2"));
     }
 }
